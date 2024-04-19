89d5925011eb58aacd0a8d5ed52f2295171d874a
Bilal Ahmed <bilalkietacc@gmail.com>
2024-04-19 15:23:49 +0500

    update note endpoint created

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -45,4 +45,45 @@ router.post(
     }
   }
 );
+
+//update note, PUT /api/notes/update
+router.put(
+  "/update/:id",
+  fetchUser,
+  [
+    body("title", "title can not be blank").not().isEmpty(),
+    body("description", "description can not be blank").not().isEmpty(),
+    body("tag", "tag can not be blank").not().isEmpty(),
+  ],
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors });
+      }
+      let note = await Note.findById(req.params.id);
+      if (!note) {
+        return res.status(404).send("Note Not Found!");
+      }
+      if (req.user.id !== note.user.toString()) {
+        return res.status(401).send("UnAuthorized Access!");
+      }
+      const { title, description, tag } = req.body;
+      const updatedNote = {
+        title,
+        description,
+        tag,
+      };
+      note = await Note.findByIdAndUpdate(
+        req.params.id,
+        { $set: updatedNote },
+        { new: true }
+      );
+      res.send("note updated");
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal Server Error!");
+    }
+  }
+);
 module.exports = router;
