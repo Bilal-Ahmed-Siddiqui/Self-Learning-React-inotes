c018db7e918dace63d23674e778b6fc7b7e90b60
bilal Ahmed <bilalkietacc@outlook.com>
2024-04-19 15:03:30 +0500

    note schema update and fetch all notes endpoint created

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const fetchUser = require("../middleware/fetchUser");
+const Note = require("../models/Note");
+const { body, validationResult } = require("express-validator");
 
-router.get("/", (req, res) => {
-  res.json([]);
+//fetchall notes GET, /api/notes/fetchall
+router.get("/fetchall", fetchUser, async (req, res) => {
+  try {
+    const notes = await Note.find({ user: req.user.id });
+    res.json({ notes });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error!");
+  }
 });
+
 
 module.exports = router;
