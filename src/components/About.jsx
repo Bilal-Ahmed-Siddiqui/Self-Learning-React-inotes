72474e44d788347ffc62aeb424a42829251b68f3
bilal Ahmed <bilalkietacc@outlook.com>
2024-04-20 13:11:06 +0500

    react-router-dom integration

---
+++ src/components/About.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+const about = () => {
+  return (
+    <>
+      <Navbar></Navbar>
+      <div>about page</div>
+    </>
+  );
+};
+
+export default about;
---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "./Navbar";
+const Home = () => {
+  return (
+    <>
+      <Navbar></Navbar>
+      <div>home page</div>
+    </>
+  );
+};
+
+export default Home;
