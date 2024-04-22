59f3197a3dae01e4eb58164b2e63356a92b1941e
bilal Ahmed <bilalkietacc@outlook.com>
2024-04-22 16:07:50 +0500

    notesContext

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import Home from "./components/Home";
 import Login from "./components/Login";
 import About from "./components/About";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import notesContext, { notesState } from "./contexts/notesContext";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" exact element={<Home />} />
-        <Route path="/login" exact element={<Login />} />
-        <Route path="/about" exact element={<About />} />
-      </Routes>
-    </Router>
+    <notesState>
+      <Router>
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/login" exact element={<Login />} />
+          <Route path="/about" exact element={<About />} />
+        </Routes>
+      </Router>
+    </notesState>
   );
 }
 
---
+++ src/contexts/notesContext.js
@@ -0,0 +1,35 @@
+import { createContext, useState } from "react";
+
+const notesContext = createContext();
+
+export const NotesState = (props) => {
+  const initialState = [
+    {
+      _id: "662240ef999f6637dd9561de",
+      user: "6622231f22a3929feddf7b34",
+      title: "abc",
+      description: "abc",
+      tag: "abc",
+      date: "2024-04-19T10:01:19.751Z",
+      __v: 0,
+    },
+    {
+      _id: "662241329b945f2403d38fb4",
+      user: "6622231f22a3929feddf7b34",
+      title: "abc",
+      description: "abc",
+      tag: "abc",
+      date: "2024-04-19T10:02:26.269Z",
+      __v: 0,
+    },
+  ];
+
+  const [notes, setnotes] = useState(initialState);
+  return (
+    <notesContext.Provider value={{ notes, setnotes }}>
+      props.childern
+    </notesContext.Provider>
+  );
+};
+
+export default notesContext;
